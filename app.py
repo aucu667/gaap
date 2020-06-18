090314c295760d3351fe0c10e12460f53deb26a1
aucu667 <aucu667@gmail.com>
2020-06-18 17:18:28 +0900

    main html addition

--- app.py
+++ app.py
@@ -14,9 +14,22 @@ def home():
    return render_template('index.html')
 
 @app.route('/kifrs1001', methods=['GET'])
-def listing():
+def listingkifrs1001():
     result = list(db.kifrs1001.find({}, {'_id':0}).sort('no'))  
     return jsonify({'result':'success', 'msg':'GET 연결되었습니다!', 'standards':result})
+
+@app.route('/kifrs1002', methods=['GET'])
+def listingkifrs1002():
+    result = list(db.kifrs1002.find({}, {'_id':0}).sort('no'))  
+    return jsonify({'result':'success', 'msg':'GET 연결되었습니다!', 'standards':result})
+
+@app.route('/kifrs1007', methods=['GET'])
+def listingkifrs1007():
+    result = list(db.kifrs1007.find({}, {'_id':0}).sort('no'))  
+    return jsonify({'result':'success', 'msg':'GET 연결되었습니다!', 'standards':result})
+
+
+
 
 if __name__ == '__main__':
    app.run('0.0.0.0',port=5000,debug=True)
